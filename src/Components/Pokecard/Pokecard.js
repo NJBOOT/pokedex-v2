edff544b87e158652ce0ba0422627b3280789ced
Nico Barbarese <nicobarbarese@gmail.com>
2021-04-22 09:07:22 -0400

    added type

--- src/Components/Pokecard/Pokecard.js
+++ src/Components/Pokecard/Pokecard.js
@@ -6,7 +6,13 @@ const Pokecard = props => {
     <div className="Pokecard">
       <h2>{props.name}</h2>
       <img src={props.image} alt={`Name: ${props.name}`} />
-      <h3>Exp: {props.base_exp}</h3>
+      <h4>
+        <span style={{ fontWeight: "800" }}>TYPE:</span>{" "}
+        <span style={{ textTransform: "capitalize" }}>{props.type}</span>
+      </h4>
+      <h4>
+        <span style={{ fontWeight: "800" }}>EXP:</span> {props.base_exp}
+      </h4>
     </div>
   );
 };
